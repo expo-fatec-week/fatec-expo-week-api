ff33a97b952d3b7c8ad76b599b6f4ca8b1305853
Luis <luisfelipe.sb@outlook.com>
2022-10-02 01:02:37 -0300

    feat(jwt): create and apply jwt configs

---
+++ src/helpers/login.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+function generateToken(idUser, userName) {
+
+  const secret = process.env.SECRET_JWT;
+
+  return jwt.sign({infoUser: {idUser, userName}}, secret, {expiresIn: 60 * 60 * 24});
+}
+
+export {generateToken};
