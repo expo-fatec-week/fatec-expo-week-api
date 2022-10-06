52028e295a0fc09e55c8ed7c55e4b9411f3dcdea
GabrielMullerDS <gabhmuller10@gmail.com>
2022-10-06 00:19:04 -0300

    Continuation of agendaController

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -3,15 +3,14 @@ import db from '../services/aegndaService.js';
 
 const router = express.Router();
 
-router.post('/', [/*Middleware se necessário*/], async (request, response) => {
+router.post('/', async (request, response) => {
   try {
-    const {idEvento, idPessoa} = request.body;
-    const {validacao} = 0;
+    const { idEvento, idPessoa } = request.body;
+    const { validacao } = 0;
+
+    await db.setAgenda(idEvento, idPessoa, validacao);
     response.status(201).json('Evento registrado na agenda.');
+  } catch (err) {
+    response.status(500).json({ message: `Encontramos um erro: ${err}` });
   }
-  catch(err) {
-  /*‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\
-  | (Müller) vou terminar em     |
-  | casa.                        |
-  \_____________________________*/
-  })
+});
