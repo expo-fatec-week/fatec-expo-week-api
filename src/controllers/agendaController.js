f86bedac7237581b1eb3c2f8147e314d0137b922
GabrielMullerDS <gabhmuller10@gmail.com>
2022-10-08 00:50:30 -0300

    Update in agendaService and agendaController

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -4,13 +4,30 @@ import db from '../services/aegndaService.js';
 const router = express.Router();
 
 router.post('/', async (request, response) => {
-  try {
-    const { idEvento, idPessoa } = request.body;
-    const { validacao } = 0;
 
+  const { idEvento, idPessoa } = request.body;
+  const { validacao } = 0;
+
+  try {
     await db.setAgenda(idEvento, idPessoa, validacao);
     response.status(201).json('Evento registrado na agenda.');
   } catch (err) {
     response.status(500).json({ message: `Encontramos um erro: ${err}` });
+  }
+});
+router.put('/', async (request, response) => {
+
+  const { idEvento, idPessoa, qmValidou, dataHora } = request.body;
+  const { validacao } = 1
+  try {
+    const results = await db.selectAgenda(idPessoa)
+    if (results > 15) {
+      await db.upAgenda(idEvento, idPessoa, validacao, qmValidou, dataHora);
+      response.status(201).json('Validação feita com sucesso!');
+    } else {
+      response.status(406/*Not Acceptable*/).json('Não faz tanto tempo que essa pessoa assistiu a um evento, peça que retorne mais tarde!');
+    }
+  } catch(err){
+    response.status(500).json(`Houve um erro: ${err}`);
   }
 });
--- src/services/agendaService.js
+++ src/services/agendaService.js
@@ -2,10 +2,25 @@ import database from '../repository/connection.js';
 
 async function setAgenda(idEvento, idPessoa, validacao){
     const conn = await database.connect();
-    const sql = 'insert into agenda(id_evento, id_pessoa, validacao, dtcria) values(?, ?, ?, ?, now());';
+    const sql = 'insert into agenda(id_evento, id_pessoa, validacao, dtcria) values(?, ?, ?, now());';
     const dataAgenda = [idEvento, idPessoa, validacao];
     await conn.query(sql, dataAgenda);
+    conn.end();
 }
-async function upAgenda(){}
+async function upAgenda(idEvento, idPessoa, validacao, qmValidou, dataHora){
+    const conn = await database.connect();
+    const sql = 'update agenda set validacao = ?, quem_validou = ?, data_hora = now() where id_evento = ? and id_pessoa = ?;';
+    const data_upAgenda = [validacao, qmValidou, dataHora, idEvento, idPessoa];
+    await conn.querry(sql, data_upAgenda);
+    conn.end();
+}
+async function selectAgenda(idPessoa){
+    const conn = await database.connect();
+    const sql = 'select max(data_hora) dataHora from agenda where id_pessoa = ?;';
+    const data_upAgenda = [idPessoa];
+    const [rows] = await conn.query(sql, data_upAgenda);
+    conn.end();
+    return [rows]
+}
 
-export default {setAgenda, upAgenda}
+export default {setAgenda, upAgenda, selectAgenda}
