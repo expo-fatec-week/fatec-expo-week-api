0705bf05cf4b49d62796ff1f65dae418097df625
Luis <luisfelipe.sb@outlook.com>
2022-10-19 19:45:02 -0300

    feat(checkout): comprove checkout to users

---
+++ src/controllers/checkoutController.js
@@ -0,0 +1,35 @@
+import express from "express";
+import db from "../services/checkoutService.js";
+
+const router = express.Router();
+
+router.get('/', async (request, response) => {
+
+  const results = await db.findEventCheckout(7);
+
+  try {
+    if (results.length == 0) {
+      response.status(204).end();
+    } else {
+      response.status(200).json(results);
+    }
+  } catch (err) {
+    response.status(500).json({ message: `Encontramos um erro: ${err}` });
+  }
+});
+
+router.put('/', async (request, response) => {
+
+  const {code, person} = request.body;
+
+  try {
+    person.forEach(async (idPerson) => {
+      await db.confirmEventCheckout(code, idPerson);
+    });
+    response.status(200).json({message: 'Obrigado por sua participação'});
+  } catch (err) {
+    response.status(500).json({ message: `Encontramos um erro: ${err}` });
+  }
+});
+
+export default router;
---
+++ src/services/checkoutService.js
@@ -0,0 +1,20 @@
+import database from '../repository/connection.js';
+
+async function findEventCheckout(code) {
+  const conn = await database.connect();
+  const sql = 'SELECT * FROM vw_valida_estande WHERE id_evento = ? AND validacao = 0';
+  const dataEvent = [code];
+  const [rows] = await conn.query(sql, dataEvent);
+  conn.end();
+  return rows;
+}
+
+async function confirmEventCheckout(code, idPerson) {
+  const conn = await database.connect();
+  const sql = `UPDATE agenda SET validacao = 1, data_hora = now(), quem_validou = 'teste' WHERE id_evento = ? AND id_pessoa = ?`;
+  const dataConfirm = [code, idPerson];
+  await conn.query(sql, dataConfirm);
+  conn.end();
+}
+
+export default { findEventCheckout, confirmEventCheckout }
