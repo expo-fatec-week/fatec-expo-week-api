175b85bf26f1452c4809c0e4a760ce8d8a5b9d14
Wesley S. Batista <wesleysb07@gmail.com>
2022-10-24 19:38:24 -0300

    Desenvolvimento do metodo que consulta os eventos assistidos pelo aluno.

--- src/controllers/agendaController.js
+++ src/controllers/agendaController.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/', async (request, response) => {
 
   const { eventId, userId } = request.body;
-  const validacao  =  0;
+  const validacao = 0;
 
   try {
     eventId.forEach(async (event) => {
@@ -39,15 +39,32 @@ router.get('/:userId', async (request, response) => {
   const { userId } = request.params;
 
   try {
-      const results = await db.findEvent(userId);
+    const results = await db.findEvent(userId);
 
-      if (results.length == 0) {
-          response.status(204).end();
-      } else {
-          response.status(200).json(results);
-      }
+    if (results.length == 0) {
+      response.status(204).end();
+    } else {
+      response.status(200).json(results);
+    }
   } catch (err) {
-      response.status(500).json({ message: `Encontramos um erro: ${err}` });
+    response.status(500).json({ message: `Encontramos um erro: ${err}` });
+  }
+});
+
+router.get('/:userId/viewed-events', async (request, response) => {
+
+  const { userId } = request.params;
+
+  try {
+    const results = await db.listViewedEvents(userId);
+
+    if (results.length === 0) {
+      response.status(204).end();
+    } else {
+      response.status(200).json(results);
+    }
+  } catch (err) {
+    response.status(500).json({ message: `Encontramos um erro: ${err}` });
   }
 });
 
